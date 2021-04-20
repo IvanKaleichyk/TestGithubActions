cf6403595a1ab5f2edea97caec0c3fdb101fffe5
Ivan <64867022+KoleychikIvan@users.noreply.github.com>
2021-04-20 12:13:08 +0300

    test for unit test

--- app/src/test/java/com/kaleichyk/testgithubactions/ExampleUnitTest.kt
+++ app/src/test/java/com/kaleichyk/testgithubactions/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.kaleichyk.testgithubactions
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +13,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun test() {
+        assertEquals(4, 2 + 3)
+    }
+
 }
